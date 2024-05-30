f19d5a34c23160a042290175459d20fda67c0b11
sinclaircombs <90660007+sinclaircombs@users.noreply.github.com>
2024-05-30 09:28:59 -0600

    Add files via upload

---
+++ SRC_files/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='py_figures',
+   url='https://github.com/Maughan-Lab/py_figures',
+   version='1.0',
+   author='Sinclair R. Combs',
+   author_email='sinclaircombs@mines.edu',
+   packages=['py_figures'],
+   description='Python package for generate XRD, PDF, EIS, and CP plots'
+)
